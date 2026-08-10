d5d42619d043bba0fef428aed755174cf3568175
Anand Sonwane <anand.sonwane123@gmail.com>
2026-08-10 11:58:30 +0530

    initial code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Headers from "./Headers"
+function App(){
+  return(
+    <div>
+      <Headers />
+      <h1>Hello</h1>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/Headers.jsx
@@ -0,0 +1,7 @@
+function Headers(){
+    return <div>
+        <h4>Headers</h4>
+    </div>
+}
+
+export default Headers;
